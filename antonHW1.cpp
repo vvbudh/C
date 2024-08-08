a719602c4cd7143f026ea0c733aebc3839d9a79b
vvbudh <vvbudh@gmail.com>
2024-08-08 05:29:58 -0700

    Got it all working, added a new triangle2 struct, as well as got them to sum together. Commented out some of the code, and added a few comments. Looks like it all works.

--- antonHW1.cpp
+++ antonHW1.cpp
@@ -4,22 +4,30 @@ using namespace std;
 struct triangle
 {
 	int sides[3];
-	int area;
+	double area;
 };
 
-int getUserInputSides(int& a, int& b, int& c);
-void calculateArea(int a, int b, int c);
+void getUserInputSides(int& a, int& b, int& c);
+double calculateArea(int a, int b, int c, double& d);
 
 int main()
 {	
 	/*zero out the sides and angles. Also instantiate the first
 	 triangle. */
+	//Double that holds it all.
+	double sumOfTriangles = 0;
+	
 	triangle triangle1;
 	triangle1.sides[0] = 0;
 	triangle1.sides[1] = 0;
 	triangle1.sides[2] = 0;
-	triangle1.area = 0;
-
+	triangle1.area = 0.0;
+	
+	triangle triangle2;
+	triangle2.sides[0] = 0;
+	triangle2.sides[1] = 0;
+	triangle2.sides[2] = 0;
+	triangle2.area = 0.0;
 	//make the tmp vars that are passed to the function GetUserInput.
 	//Do I have to do it this way? How do I get these ones passed back to
 	//the main namespace? without creating 3 extras. That't what I have
@@ -33,20 +41,30 @@ int main()
 	//Use the Get User Input sides function to get the length of the sides.
 	getUserInputSides(triangle1.sides[0], triangle1.sides[1] , triangle1.sides[2]);
 	
-	//set the variable  to the value of the cin "a" variable.
+	//set the variable  to the value of the cin "a" variable. Below is just output to verify it works.
 	//triangle1.sides[0] = a;
 	//triangle1.sides[1] = b;
 	//triangle1.sides[2] = c;
-	for(int i=0;i<3;i++)
+	/*for(int i=0;i<3;i++)
 	{
 		cout << triangle1.sides[i] << endl;
-	}
-	
-	calculateArea(triangle1.sides[0], triangle1.sides[1] , triangle1.sides[2]);
+	}*/
+	//Get user input on second triangle.
+	getUserInputSides(triangle2.sides[0], triangle2.sides[1] , triangle2.sides[2]);
+
+	/*for(int i=0;i<3;i++)
+	{
+		cout << triangle2.sides[i] << endl;
+	}*/
+	calculateArea(triangle1.sides[0], triangle1.sides[1] , triangle1.sides[2], triangle1.area);
+	calculateArea(triangle2.sides[0], triangle2.sides[1] , triangle2.sides[2], triangle2.area);
+
+	sumOfTriangles = triangle1.area + triangle2.area;
+	cout << endl << sumOfTriangles << endl;
 }
 
 
-int getUserInputSides(int& a, int& b, int& c)
+void getUserInputSides(int& a, int& b, int& c)
 {	/* make the string to tell the user give inputs for the things
 	put them in the array.*/
 	string toUser = "Hey give me one number.";
@@ -58,6 +76,7 @@ int getUserInputSides(int& a, int& b, int& c)
 	//int good = 0;
 
 	//print to user the prompt.
+			//Make error checking later.
 	cout << toUser;
 	//get their input.
 	//cin >> all3Sides[0];
@@ -68,12 +87,11 @@ int getUserInputSides(int& a, int& b, int& c)
 	cin >> c;
 	//make function that checks value of the input.
 	//return all3Sides[0];
-	return a;
 }
 
 
 
-void calculateArea(int a, int b, int c)
+double calculateArea(int a, int b, int c, double& d)
 {
 	//Need to use Heron's formula apparently.
 /*	int s = 0;
@@ -99,14 +117,15 @@ cout << endl << heron << endl;
 	//divide by 2.0 so it works, and isn't division by integers. 
 	//also uhh, a b c vals are 5,7,8 and should come out to 10Sum and 17.3Area
 	double sum = 0.0;
-	double area = 0.0;
+	//Replace with d pass by reference var.
+	//double area = 0.0;
 
 	sum = (a + b + c)/2.0;
-	cout << sum << endl;
-	area = sqrt((sum)*(sum-a)*(sum-b)*(sum-c));
-	cout << area << endl;
+	//cout << sum << endl; // checking code, it sums the above at it should.
+	d = sqrt((sum)*(sum-a)*(sum-b)*(sum-c));
+	//area = sqrt((sum)*(sum-a)*(sum-b)*(sum-c));
+	//cout << area << endl;
+	//return area;
+	return d;
 
-
-
-	//sqrt(s(s-a)(s-b)(s-c));
 };
