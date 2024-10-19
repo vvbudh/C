97f498720664be982e8e884e49ae74d34b989e3f
vvbudh <vvbudh@gmail.com>
2024-10-18 20:08:19 -0700

    Got it all working, Had some help from the AI and https://www.geeksforgeeks.org/how-to-validate-user-input-in-cpp/ I was able to create the exit menu and it's idiot proofed.  Works good.

--- BlindJump/main.cpp
+++ BlindJump/main.cpp
@@ -7,44 +7,83 @@
 //Get time from PC To have pseudo random set of data. Also to allow save scumming.
 #include <ctime>
 #include "ShipCharacteristics.h"
-using namespace std;
+//using namespace std; //I know.
 
-
+int pickShip();
 
 int main()
 {
     int GAMERUNNING = 1;
     while(GAMERUNNING == 1)
+    {   
+//https://www.geeksforgeeks.org/how-to-validate-user-input-in-cpp/
+
+    int userInputMainMenu = -1;
+    std::cout << "0 for Exit, 1 for Pick Ship. 3 for Load Save, 4 for Blank." << std::endl;
+        ///Might need to change this a bit. I want it to exit after one ship selection and move on.
+        while ((!(std::cin >> userInputMainMenu)))
+        {
+        std::cout << "Invalid input. Please enter a number: " << std::endl;
+        std::cout << "0 for Exit, 1 for Pick Ship. 3 for Load Save, 4 for Blank." << std::endl;
+        std::cin.clear(); 
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+
+
+    if(userInputMainMenu == 0)
     {
-
-        //Pick what ship.
-        std::mt19937 gen(time(0));
-        int weak = 1; int strong = 5; //Increase to make more space for ship class.
-        int randomGenShip(std::mt19937& gen, int weak, int strong);
-        std::uniform_int_distribution<> distrib(weak, strong);
-        int shipClass = distrib(gen);
-
-        if (shipClass == 1)
-        {Corvette pirannah;
-        pirannah.printStatusReport();
-        }
-        if (shipClass == 2)
-        {Frigate mandrake;
-        mandrake.printStatusReport();
-        }
-        if (shipClass == 3)
-        {Destroyer doombsday;
-        doombsday.printStatusReport();
-        }
-        if (shipClass == 4)
-        {BattleCruiser hornet;
-        hornet.printStatusReport();
-        }
-        if (shipClass == 5)
-        {Carrier lord;
-        lord.printStatusReport();
-        }
+        std::cout << "Exiting the game..." << std::endl;
         GAMERUNNING = 0;
+    }
+    else if(userInputMainMenu == 1)
+    {
+        std::cout << "Choosen 1" << std::endl;
+        pickShip();
+    }
+    else if(userInputMainMenu == 2)
+    {
+        std::cout << "Choosen 2" << std::endl;
+    }
+    else if(userInputMainMenu == 3)
+    {
+        std::cout << "Choosen 3" << std::endl;
+    }
+    else if(userInputMainMenu == 4)
+    {
+        std::cout << "Choosen 4" << std::endl;
+    }
+
     }
     return 0;
 }
+
+int pickShip()
+{
+//Pick what ship.
+    std::mt19937 gen(time(0));
+    int weak = 1; int strong = 5; //Increase to make more space for ship class.
+    int randomGenShip(std::mt19937& gen, int weak, int strong);
+    std::uniform_int_distribution<> distrib(weak, strong);
+    int shipClass = distrib(gen);
+
+    if (shipClass == 1)
+    {Corvette pirannah;
+    pirannah.printStatusReport();
+    }
+    if (shipClass == 2)
+    {Frigate mandrake;
+    mandrake.printStatusReport();
+    }
+    if (shipClass == 3)
+    {Destroyer doombsday;
+    doombsday.printStatusReport();
+    }
+    if (shipClass == 4)
+    {BattleCruiser hornet;
+      hornet.printStatusReport();
+    }
+    if (shipClass == 5)
+    {Carrier lord;
+    lord.printStatusReport();
+    }
+};
